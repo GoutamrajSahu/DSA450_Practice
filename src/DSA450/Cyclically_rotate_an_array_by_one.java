cd2f7f8232e203a79aa8d017a8d270ee5777d684
GoutamrajSahu <sahugoutamraj@gmail.com>
2022-07-17 18:50:42 +0530

    Cyclically_rotate_an_array

--- src/DSA450/Cyclically_rotate_an_array_by_one.java
+++ src/DSA450/Cyclically_rotate_an_array_by_one.java
@@ -1,4 +1,22 @@
 package DSA450;
 
 public class Cyclically_rotate_an_array_by_one {
+    Cyclically_rotate_an_array_by_one(){
+        int arr1[] = {1, 2, 3, 4, 5};
+        int []res = new int[arr1.length];
+        int shift = 1;
+        if(shift >= arr1.length){
+            shift = shift % arr1.length;
+        }
+        int j=0;
+        for(int i = shift; i< arr1.length; i++, j++){
+            res[j] = arr1[i];
+        }
+        for(int i = 0; i<shift; i++,j++){
+            res[j] = arr1[i];
+        }
+        for (int ele: res) {
+            System.out.print(ele+" ");
+        }
+    }
 }
