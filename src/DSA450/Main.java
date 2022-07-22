2bef6e11760c5343e8a09b444487285a80e583cb
GoutamrajSahu <sahugoutamraj@gmail.com>
2022-07-23 00:07:44 +0530

    Largest contiguous sum

--- src/DSA450/Main.java
+++ src/DSA450/Main.java
@@ -1,5 +1,7 @@
 package DSA450;
 
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class Main {
 //    new Move_all_negative_ele_to_oneside_of_an_array();
 //    new Union_and_Intersection_of_two_sorted_arrays();
 //    new Cyclically_rotate_an_array_by_one();
-      new Find_largest_sum_contiguous_subarray();
+//      new Find_largest_sum_contiguous_subarray();
+       new Minimise_the_maximum_difference_between_heights();
+        /*HashSet<String> a = new HashSet<>();
+        a.add("asd");
+        a.add("sdg sf");
+        a.add("sdg sf");
+        System.out.println(a);*/
     }
 }
--- src/DSA450/Minimise_the_maximum_difference_between_heights.java
+++ src/DSA450/Minimise_the_maximum_difference_between_heights.java
@@ -1,7 +1,37 @@
 package DSA450;
 
+import java.util.*;
+
 public class Minimise_the_maximum_difference_between_heights {
     Minimise_the_maximum_difference_between_heights(){
+        int arr[] = {2, 6 ,3 ,4 ,7 ,2 ,10 ,3 ,2 ,1};
+//        System.out.println(new HashSet<>(Arrays.asList(2, 6 ,3 ,6 ,3 ,2 ,10 ,3 ,2 ,3)));
+        int k = 5;
+        int []res = new int[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i]< k){
+                res[i] = arr[i]+k;
+            }else if(arr[i] == k){
+                res[i] = arr[i]+k;
+            }else if(arr[i] > k){
+                res[i] = arr[i]-k;
+            }
+        }
+        int min = res[0];
+        int max = res[0];
+        // for (int ele:
+        //      res) {
+        //     System.out.print(ele+" ");
+        // }
+        for(int i=1; i<res.length; i++){
+            if(min>res[i]){
+                min = res[i];
+            }
+            if(max<res[i]){
+                max = res[i];
+            }
+        }
 
+        System.out.println(max-min);
     }
 }
